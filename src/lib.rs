992743f1dcf5a0b55b5070368625e556effdde43
Keming <kemingy94@gmail.com>
2019-05-31 16:55:13 +0800

    add src

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+/*!
+This crate provides a library for text preprocessing, including tags(HTML, URL, Email) extraction
+and removing, punctuation normalization, language extraction. It aims to provide an easy-to-use
+text processing toolkit.
+*/
+
+#![deny(missing_docs)]
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
